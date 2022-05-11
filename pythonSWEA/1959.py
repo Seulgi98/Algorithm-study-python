3c5d90829fa937e64bb9214d2f4cfc55520c3133
Seulgi <hanny1226@naver.com>
2022-05-11 16:39:29 +0900

    1959.

--- pythonSWEA/1959.py
+++ pythonSWEA/1959.py
@@ -11,9 +11,9 @@ for tc in range(1, 1 + T):
 
     for i in range (M-N+1):
         temp = 0
-        for j in range (N):
+        for j in range (N): # N의 길이가 더 짧으므로 N의 전체 길이에 맞추어 인덱스가 들어감
             temp += A[j] * B[j+i]
-        if temp > maxSum:
+        if temp > maxSum: # temp가 더 크면 무조건 max로 담아준다
             maxSum = temp
 
     print('#{} {}'.format(tc, maxSum))
