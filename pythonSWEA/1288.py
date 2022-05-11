239987bd65fef4b6652595be8284000fed27a3a4
Seulgi <hanny1226@naver.com>
2022-05-11 11:28:22 +0900

    1288.

--- pythonSWEA/1288.py
+++ pythonSWEA/1288.py
@@ -1,18 +1,19 @@
 T = int(input())
+
 for tc in range(1, T+1) :
     N = int(input())
-    listA = [0]*10
-    # print('listA',listA)
+
+    numCountList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
     i = 0
     while(True) :
-        if 0 not in listA :
+        if 0 not in numCountList :
             break
         else :
             i += 1
             num = str(N*i)
-            for j in range(len(num)) :
-                # print('i', i, 'j', j)
-                listA[int(num[j])] = 1
-                # print('listA', listA)
+            for j in num :
+                numCountList[int(j)] = 1
+
 
     print("#{} {}".format(tc, num))
