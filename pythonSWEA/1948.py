13f0fb7c43637d678b2cc55ab6e0d7cb28411dd0
Seulgi <hanny1226@naver.com>
2022-05-16 10:07:08 +0900

    1948.

--- pythonSWEA/1948.py
+++ pythonSWEA/1948.py
@@ -1,6 +1,7 @@
+T= int(input())
 months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-for tc in range(1, int(input())+1):
+for tc in range(1, T+1):
     m1, d1, m2, d2 = map(int, input().split())
     ans = 0
 
