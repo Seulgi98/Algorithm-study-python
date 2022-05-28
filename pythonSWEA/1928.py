8e0fb0c1ea96e42c010a556d9a2673d3592c3265
Seulgi <hanny1226@naver.com>
2022-05-28 23:17:14 +0900

    1928.

--- pythonSWEA/1928.py
+++ pythonSWEA/1928.py
@@ -12,15 +12,16 @@ for tc in range(1, 1 + T):
     res = ''
 
     for q in range(length):
-        num = decode.index(q) # 디코드 안의 q의 인덱스
-        bin_num = str(bin(num)[2:])
+        num = decode.index(word[q]) # 디코드 안의 q의 인덱스
+        bin_num = str(bin(num)[2:]) # 0b지우려고
+        print(bin_num)
         while len(bin_num) < 6: # 6자리수가 될때까지 0붙이기 반복
             bin_num = '0' + bin_num
         res += bin_num
     r = '' # 문제에서 구하고자 하는 원래 문장
 
     for w in range(len(res) // 8):
-        e = int(res[w * 8:w * 8 + 8], 2)
-        r += chr(e)
+        e = int(res[w * 8:w * 8 + 8], 2) #int에 2를 붙이면 이진수로 바꿔준다
+        r += chr(e) #2진수를 아스키코드로 바꿔준다 (문자 abcd.....)
 
     print('#{} {}'.format(tc, r))
